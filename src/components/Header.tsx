91ece3187a01d2b50b2080c16360f2f6b5e21b23
Marveeeen <jmvroqueee@gmail.com>
2024-07-05 21:31:10 +0800

    feature: added header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import logoImg from "../assets/quiz-complete.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logoImg} alt="app logo" />
+      <h1>Quiz App</h1>
+    </header>
+  );
+};
+
+export default Header;
